54a23d78d1f136c9e1cb2c3925998534117a3aa1
Sczygelski <avamariescz@gmail.com>
2022-10-21 18:50:09 -0500

    manifest.json

--- client/src/src-sw.js
+++ client/src/src-sw.js
@@ -28,4 +28,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({ 
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+)
